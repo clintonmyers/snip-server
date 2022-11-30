e00ca263ccee381126b6f3f1c59568090c6a0601
Clint Myers <clint.r.myers@gmail.com>
2022-11-30 15:54:41 -0600

    Modified shuffle utility to be generic.

--- src/utilities/shuffle.ts
+++ src/utilities/shuffle.ts
@@ -1,6 +1,6 @@
-const shuffle = (deck: Card[]): Card[] => {
-  const unshuffled = deck;
-  const shuffled: Card[] = [];
+const shuffle = <T>(arr: T[]): T[] => {
+  const unshuffled = arr;
+  const shuffled: T[] = [];
   while (unshuffled.length) {
     const i = randomIndex(unshuffled);
     const item = unshuffled.splice(i, 1)[0];
